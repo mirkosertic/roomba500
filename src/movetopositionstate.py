4eb0f1cb98ae9e145af4d61281bb7f18790bb03c
mirkosertic <mirko.sertic@web.de>
2021-12-06 17:57:19 +0100

    Working on a simple local planner

--- src/movetopositionstate.py
+++ src/movetopositionstate.py
@@ -29,9 +29,9 @@ class MoveToPositionState(BaseState):
         # We calculate the distance to the target position
         deltaX = targetPosition.x - currentPosition.x
         deltaY = targetPosition.y - currentPosition.y
-        self.lastDistance = math.sqrt(deltaX * deltaX + deltaY * deltaY)
+        distance = math.sqrt(deltaX * deltaX + deltaY * deltaY)
 
-        rospy.loginfo("DeltaX = %s, DeltaY = %s, Distance = %s", deltaX, deltaY, self.lastDistance)
+        rospy.loginfo("DeltaX = %s, DeltaY = %s, Distance = %s", deltaX, deltaY, distance)
 
 
     def setDriveSpeed(self, targetSpeed, targetAngularMoment):
@@ -59,25 +59,19 @@ class MoveToPositionState(BaseState):
 
         odometryDegrees = self.toDegrees(odomyaw)
 
-        angleDelta = degreesToTarget - odometryDegrees
+        relativeAngle = odometryDegrees - degreesToTarget
 
-        rospy.loginfo("DeltaX = %s, DeltaY = %s, Distance = %s, Angle to Target = %s, Odometry Angle = %s, Delta Angle = %s", deltaX, deltaY, distance, degreesToTarget, odometryDegrees, angleDelta)
+        rospy.loginfo("Distance = %s, Relative Angle = %s, Odometry angle = %s, Target Angle = %s", distance, relativeAngle, odometryDegrees, degreesToTarget)
 
-        # Either we are close to the target position or we overshot (distance is larger than last known distance)
-        if (distance < 0.02 or (self.lastDistance - distance > 0.02)):
+        # Either we are close to the target position or we overshot
+        # We are at the nearest point if either the distance is near a reasonable limit
+        # or the angle to the target point is about 90 degrees
+        if (distance <= 0.01 or abs(relativeAngle) >= 90.0):
             # We are near the right place
-            rospy.loginfo("Stopping due to distance = %s, last distance = %s", distance, self.lastDistance)
+            rospy.loginfo("Stopping, as we can't get nearer to the desired point")
 
             self.pathmanager.driver.stop()
             return self.success()
-
-        if (abs(angleDelta) > 20):
-            # There is something wrong with our heading, we need to realign
-            # TODO: Implement realign
-            rospy.loginfo("Stopping due angle misalignment with delta = %s", angleDelta)
-
-            self.pathmanager.driver.stop()
-            return self.error()
 
         #
         # Positive Angle Delta -> Rotate left, else right
@@ -96,22 +90,21 @@ class MoveToPositionState(BaseState):
             rospy.loginfo("Comming closer, slowing down")
             multiplier = 0.5
 
-        if (angleDelta > 0):
-            if (angleDelta > 1):
-                rospy.loginfo("Correcting heading by turning to left")
-                rotationSpeed = 0.25
-            else:
-                rospy.loginfo("No heading correction required")
-        if (angleDelta < 0):
-            if (angleDelta < 1):
-                rospy.loginfo("Correcting heading by turning to right")
-                rotationSpeed = -0.25
-            else:
-                rospy.loginfo("No heading correction required")
+        #if (angleDelta > 0):
+        #    if (angleDelta > 1):
+        #        rospy.loginfo("Correcting heading by turning to left")
+        #        rotationSpeed = 0.25
+        #    else:
+        #        rospy.loginfo("No heading correction required")
+
+        #if (angleDelta < 0):
+        #    if (angleDelta < 1):
+        #        rospy.loginfo("Correcting heading by turning to right")
+        #        rotationSpeed = -0.25
+        #    else:
+        #        rospy.loginfo("No heading correction required")
 
         self.setDriveSpeed(driveSpeed * multiplier, rotationSpeed)
-
-        self.lastDistance = distance
 
         # We continue to drive, and stay in the current system state
         return self
