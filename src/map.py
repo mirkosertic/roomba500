622d017704048789f06e9117da3df93af7957cbc
mirkosertic <mirko.sertic@web.de>
2022-02-12 13:17:17 +0100

    Working on better map implementation

---
+++ src/map.py
@@ -0,0 +1,299 @@
+#!/usr/bin/env python3
+
+import enum
+import math
+
+import rospy
+import numpy as np
+
+from priorityqueue import PriorityQueue
+
+class GridCellStatus(enum.Enum):
+    UNKNOWN = 1
+    OCCUPIED = 2
+    FREE = 3
+
+class MapGridCell:
+
+    def __init__(self, centerx, centery, size, status):
+        self.centerx = centerx
+        self.centery = centery
+        self.size = size
+        self.topleft = None
+        self.top = None
+        self.topright = None
+        self.left = None
+        self.right = None
+        self.bottomleft = None
+        self.bottom = None
+        self.bottomright = None
+        self.status = status
+
+    def isCovering(self, position):
+        topleftx = self.centerx - self.size / 2
+        toplefty = self.centery + self.size / 2
+
+        bottomrightx = self.centerx + self.size / 2
+        bottomrighty = self.centery - self.size / 2
+
+        return position.x >= topleftx and position.x <= bottomrightx and position.y >= bottomrighty and position.y <= toplefty
+
+    def updateStatusFrom(self, map, scanwidthinmeters, occupancythreshold):
+        topleftx = self.centerx - scanwidthinmeters / 2
+        toplefty = self.centery + scanwidthinmeters / 2
+
+        bottomrightx = self.centerx + scanwidthinmeters / 2
+        bottomrighty = self.centery - scanwidthinmeters / 2
+
+        topleftxoc = int((topleftx - map.info.origin.position.x) / map.info.resolution)
+        topleftyoc = int((toplefty - map.info.origin.position.y) / map.info.resolution)
+
+        bottomrightxoc = int((bottomrightx - map.info.origin.position.x) / map.info.resolution)
+        bottomrightyoc = int((bottomrighty - map.info.origin.position.y) / map.info.resolution)
+
+        containsUnknowns = False
+
+        for y in range(bottomrightyoc, topleftyoc, 1):
+            for x in range(topleftxoc, bottomrightxoc, 1):
+                # Clip to valid coordinates
+                if x >= 0 and x < map.info.width and y >= 0 and y < map.info.height:
+                    value = map.data[y * map.info.width + x]
+                    if value == -1:
+                        containsUnknowns = True
+                    elif value > occupancythreshold:
+                        # As soon as there is a single occupied point on the map,
+                        # the cell is occupied
+                        self.status = GridCellStatus.OCCUPIED
+                        return
+
+        if containsUnknowns:
+            self.status = GridCellStatus.UNKNOWN
+            return
+
+        self.status = GridCellStatus.FREE
+
+class NavigationMap:
+
+    def __init__(self):
+        self.gridcellwidthinmeters = 0.34
+        self.scanwidthinmeters = 0.34
+        self.occupancythreshold = 0.65
+        self.cells = []
+        self.currentmap =  None
+
+    def isNewMap(self, map):
+        if self.currentmap is None:
+            return True
+
+        if self.currentmap.info.width != map.info.width:
+            return True
+
+        if self.currentmap.info.height != map.info.height:
+            return True
+
+        if self.currentmap.info.resolution != map.info.resolution:
+            return True
+
+        if self.currentmap.info.origin.position.x != map.info.origin.position.x:
+            return True
+
+        if self.currentmap.info.origin.position.y != map.info.origin.position.y:
+            return True
+
+        return False
+
+    def updateCellStatusFrom(self, map):
+        self.currentmap = map
+        for cell in self.cells:
+            cell.updateStatusFrom(map, self.scanwidthinmeters, self.occupancythreshold)
+
+    def initOrUpdateFromOccupancyGrid(self, map):
+
+        if not self.isNewMap(map):
+            self.updateCellStatusFrom(map)
+            return
+
+        if self.currentmap is not None:
+            rospy.loginfo("Changes in map position, size or resolution are currently not supported!")
+            return
+
+        origin = map.info.origin
+        width = map.info.width
+        height = map.info.height
+
+        mapwidthinmeters = width * map.info.resolution
+        mapheightinmeters = height * map.info.resolution
+
+        squaresperrow = len(np.arange(0, mapwidthinmeters, self.gridcellwidthinmeters))
+
+        yoffset = 0
+        for y in np.arange(0, mapheightinmeters, self.gridcellwidthinmeters):
+            xoffset = 0
+            for x in np.arange(0, mapwidthinmeters, self.gridcellwidthinmeters):
+                centerx = origin.position.x + x + (self.gridcellwidthinmeters / 2)
+                centery = origin.position.y + y + (self.gridcellwidthinmeters / 2)
+
+                cell = MapGridCell(centerx, centery, self.gridcellwidthinmeters, GridCellStatus.UNKNOWN)
+
+                # We connect the cells
+                if xoffset > 0:
+                    # Connect left cell
+                    left = self.cells[len(self.cells) - 1]
+                    cell.left = left
+                    left.right = cell
+
+                if yoffset > 0:
+                    # Bottom cell
+                    bottom = self.cells[len(self.cells) - squaresperrow]
+                    bottom.top = cell
+                    cell.bottom = bottom
+
+                if yoffset > 0 and xoffset > 0:
+                    # Bottom left
+                    bottomleft = self.cells[len(self.cells) - squaresperrow - 1]
+                    bottomleft.topright = cell
+                    cell.bottomleft = bottomleft
+
+                if yoffset > 0 and xoffset < squaresperrow - 1:
+                    # Bottom right
+                    bottomright = self.cells[len(self.cells) - squaresperrow + 1]
+                    bottomright.topleft = cell
+                    cell.bottomright = bottomright
+
+                self.cells.append(cell)
+
+                xoffset = xoffset + 1
+
+            yoffset = yoffset + 1
+
+        self.updateCellStatusFrom(map)
+
+    def nearestCellCovering(self, position):
+        nearestCell = None
+        nearestDistance = None
+        for cell in self.cells:
+            if cell.isCovering(position):
+                dx = position.x - cell.centerx
+                dy = position.y - cell.centery
+                distance = math.sqrt(dx * dx + dy * dy)
+                if nearestCell is None or distance < nearestDistance:
+                    nearestCell = cell
+                    nearestDistance = distance
+
+        return nearestCell
+
+    def findPath(self, src, target):
+
+        # Code taken from https://www.redblobgames.com/pathfinding/a-star/implementation.html
+        frontier = PriorityQueue()
+        frontier.put((id(src), src), 0)
+
+        came_from = {}
+        cost_so_far = {}
+        came_from[src] = None
+        cost_so_far[src] = 0
+
+        def compress(path):
+            result = []
+            for i, cell in enumerate(path):
+                if i == 0 or i == len(path) - 1:
+                    # We have to keep the first and the last point of the path
+                    result.append(cell)
+                else:
+                    # We check the trajectory from the previous point to the current
+                    prevCell = path[i - 1]
+                    currCell = cell
+                    nextCell = path[i + 1]
+
+                    tprev = math.atan2(currCell.centery - prevCell.centery, currCell.centerx - prevCell.centerx)
+                    tnext = math.atan2(nextCell.centery - currCell.centery, nextCell.centerx - currCell.centerx)
+
+                    if abs(tnext - tprev) > math.pi / 8:  # Change greater than 22.5 degrees
+                        result.append(cell)
+
+            return result
+
+        def heuristicscore(src, target):
+            dx = target.centerx - src.centerx
+            dy = target.centery - src.centery
+            #return math.sqrt(dx * dx + dy * dy)
+            return abs(dx) + abs(dy)
+
+        def adjecentcellsfor(src):
+            cells = []
+
+            def freeCell(cell):
+                return cell is not None and  cell.status == GridCellStatus.FREE
+
+            if freeCell(src.top):
+                cells.append(src.top)
+            if freeCell(src.bottom):
+                cells.append(src.bottom)
+            if freeCell(src.left):
+                cells.append(src.left)
+            if freeCell(src.right):
+                cells.append(src.right)
+
+            if freeCell(src.top) and freeCell(src.right) and freeCell(src.topright):
+                cells.append(src.topright)
+            if freeCell(src.bottom) and freeCell(src.right) and freeCell(src.bottomright):
+                cells.append(src.bottomright)
+            if freeCell(src.top) and freeCell(src.left) and freeCell(src.topleft):
+                cells.append(src.topleft)
+            if freeCell(src.bottom) and freeCell(src.left) and freeCell(src.bottomleft):
+                cells.append(src.bottomleft)
+
+            return cells
+
+        while not frontier.empty():
+            _, currentcell = frontier.get()
+
+            if currentcell == target:
+                completePath = []
+                while currentcell != src:
+                    completePath.append(currentcell)
+                    currentcell = came_from[currentcell]
+
+                completePath.append(src)
+                completePath.reverse()
+
+                return compress(completePath)
+
+            for nextCell in adjecentcellsfor(currentcell):
+                new_cost = cost_so_far[currentcell] + heuristicscore(currentcell, nextCell)
+                if nextCell not in cost_so_far or new_cost < cost_so_far[nextCell]:
+                    cost_so_far[nextCell] = new_cost
+                    priority = new_cost + heuristicscore(nextCell, target)
+                    frontier.put((id(nextCell), nextCell), priority)
+                    came_from[nextCell] = currentcell
+
+        return None
+
+    def toDebugImage(self):
+        image = np.zeros((self.currentmap.info.height, self.currentmap.info.width, 3), np.uint8)
+        image[:] = [0, 0, 0]
+
+        for y in range(0, self.currentmap.info.height, 1):
+            for x in range(0, self.currentmap.info.width, 1):
+                value = self.currentmap.data[y * self.currentmap.info.width + x]
+                if value == -1:
+                    color = [200, 200, 200]
+                elif value > self.occupancythreshold:
+                    color = [0, 0, 0]
+                else:
+                    color = [255, 255, 255]
+
+                image[self.currentmap.info.height - y - 1, x] = color
+
+        for cell in self.cells:
+
+            debugx = int((cell.centerx - self.currentmap.info.origin.position.x) / self.currentmap.info.resolution)
+            debugy = int((cell.centery - self.currentmap.info.origin.position.y) / self.currentmap.info.resolution)
+
+            if debugx < self.currentmap.info.width and debugy < self.currentmap.info.height:
+                if cell.status == GridCellStatus.FREE:
+                    image[self.currentmap.info.height - debugy - 1, debugx] = [0, 255, 0]
+                elif cell.status == GridCellStatus.OCCUPIED:
+                    image[self.currentmap.info.height - debugy - 1, debugx] = [0, 0, 255]
+
+        return image
---
+++ src/readmap.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python3
+
+import rospy
+import cv2
+
+from nav_msgs.srv import GetMap
+from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import PoseStamped
+from visualization_msgs.msg import Marker, MarkerArray
+from geometry_msgs.msg import Point
+from std_msgs.msg import ColorRGBA
+
+from tf.transformations import quaternion_from_euler
+
+from map import GridCellStatus, NavigationMap
+
+class ReadMap:
+
+    def __init__(self):
+        self.navigationmap = NavigationMap()
+        self.markerstopic = None
+        self.currentposition = None
+
+    def newMoveBaseSimpleGoalMessage(self, message):
+
+        quat = quaternion_from_euler(0, 0, 0)
+        markers = MarkerArray()
+
+        if self.currentposition is None:
+            current = self.navigationmap.nearestCellCovering(message.pose.position)
+            if current is not None:
+                self.currentposition = current
+
+                m1 = Marker()
+                m1.header.frame_id = 'map'
+                m1.header.stamp = rospy.Time.now()
+                m1.id = 0
+                m1.type = 1
+                m1.action = 0
+                m1.scale.x = self.navigationmap.scanwidthinmeters
+                m1.scale.y = self.navigationmap.scanwidthinmeters
+                m1.scale.z = self.navigationmap.scanwidthinmeters
+                m1.pose.position.x = self.currentposition.centerx
+                m1.pose.position.y = self.currentposition.centery
+                m1.pose.position.z = 0.0
+                m1.pose.orientation.x = quat[0]
+                m1.pose.orientation.y = quat[1]
+                m1.pose.orientation.z = quat[2]
+                m1.pose.orientation.w = quat[3]
+                m1.points = []
+                m1.colors = []
+                m1.color.a = 0.3
+                m1.color.r = 0
+                m1.color.g = 0
+                m1.color.b = 1
+
+                markers.markers.append(m1)
+            else:
+                rospy.loginfo("Cannot find initial cell near to %s:%s", message.pose.position.x, message.pose.position.y)
+        else:
+            target = self.navigationmap.nearestCellCovering(message.pose.position)
+            if target is not None:
+                path = self.navigationmap.findPath(self.currentposition, target)
+
+                m1 = Marker()
+                m1.header.frame_id = 'map'
+                m1.header.stamp = rospy.Time.now()
+                m1.id = 0
+                m1.type = 1
+                m1.action = 0
+                m1.scale.x = self.navigationmap.scanwidthinmeters
+                m1.scale.y = self.navigationmap.scanwidthinmeters
+                m1.scale.z = self.navigationmap.scanwidthinmeters
+                m1.pose.position.x = self.currentposition.centerx
+                m1.pose.position.y = self.currentposition.centery
+                m1.pose.position.z = 0.0
+                m1.pose.orientation.x = quat[0]
+                m1.pose.orientation.y = quat[1]
+                m1.pose.orientation.z = quat[2]
+                m1.pose.orientation.w = quat[3]
+                m1.points = []
+                m1.colors = []
+                m1.color.a = 0.3
+                m1.color.r = 0
+                m1.color.g = 0
+                m1.color.b = 1
+                markers.markers.append(m1)
+
+                m2 = Marker()
+                m2.header.frame_id = 'map'
+                m2.header.stamp = rospy.Time.now()
+                m2.id = 1
+                m2.type = 1
+                m2.action = 0
+                m2.scale.x = self.navigationmap.scanwidthinmeters
+                m2.scale.y = self.navigationmap.scanwidthinmeters
+                m2.scale.z = self.navigationmap.scanwidthinmeters
+                m2.pose.position.x = target.centerx
+                m2.pose.position.y = target.centery
+                m2.pose.position.z = 0.0
+                m2.pose.orientation.x = quat[0]
+                m2.pose.orientation.y = quat[1]
+                m2.pose.orientation.z = quat[2]
+                m2.pose.orientation.w = quat[3]
+                m2.points = []
+                m2.colors = []
+                m2.color.a = 0.3
+                m2.color.r = 1
+                m2.color.g = 0
+                m2.color.b = 1
+                markers.markers.append(m2)
+
+                if path is not None:
+
+                    marker = Marker()
+                    marker.header.frame_id = 'map'
+                    marker.header.stamp = rospy.Time.now()
+                    marker.id = 3
+                    marker.type = 4  # Line-strip
+                    marker.action = 0
+                    marker.scale.x = 0.05
+                    marker.pose.position.x = 0
+                    marker.pose.position.y = 0
+                    marker.pose.position.z = 0.0
+                    marker.pose.orientation.x = quat[0]
+                    marker.pose.orientation.y = quat[1]
+                    marker.pose.orientation.z = quat[2]
+                    marker.pose.orientation.w = quat[3]
+                    marker.points = []
+                    marker.colors = []
+
+                    for index, cell in enumerate(path):
+                        marker.points.append(Point(cell.centerx, cell.centery, 0.05))
+                        marker.colors.append(ColorRGBA(0, 0, 1, 1))
+
+                        p = Marker()
+                        p.header.frame_id = 'map'
+                        p.header.stamp = rospy.Time.now()
+                        p.id = 100 + index
+                        p.type = 2  # sphere
+                        p.action = 0
+                        p.scale.x = 0.15
+                        p.scale.y = 0.15
+                        p.scale.z = 0.15
+                        p.pose.position.x = cell.centerx
+                        p.pose.position.y = cell.centery
+                        p.pose.position.z = 0.0
+                        p.pose.orientation.x = quat[0]
+                        p.pose.orientation.y = quat[1]
+                        p.pose.orientation.z = quat[2]
+                        p.pose.orientation.w = quat[3]
+                        p.points = []
+                        p.colors = []
+                        p.color.a = 1
+                        p.color.r = 0
+                        p.color.g = 0
+                        p.color.b = 1
+
+                        markers.markers.append(p)
+
+
+                    marker.color.a = 1
+                    marker.color.r = 0
+                    marker.color.g = 0
+                    marker.color.b = 1
+
+                    markers.markers.append(marker)
+
+                    self.currentposition = target
+                else:
+                    rospy.loginfo("Cannot find path from %s:%s to %s:%s", self.currentposition.centerx, self.currentposition.centery, target.centerx, target.centery)
+            else:
+                rospy.loginfo("Cannot find target cell near to %s:%s", message.pose.position.x, message.pose.position.y)
+
+        if len(markers.markers) > 0:
+            self.markerstopic.publish(markers)
+
+        pass
+
+    def start(self):
+        rospy.init_node('test', anonymous=True)
+
+        rospy.loginfo('Waiting for service')
+        rospy.wait_for_service('static_map')
+        rospy.loginfo('Invoking service')
+        service = rospy.ServiceProxy('static_map', GetMap)
+        response = service()
+
+        map = response.map
+
+        self.navigationmap.initOrUpdateFromOccupancyGrid(map)
+
+        markers = MarkerArray()
+
+        for cell in self.navigationmap.cells:
+
+            if cell.status == GridCellStatus.OCCUPIED or cell.status == GridCellStatus.FREE:
+                quat = quaternion_from_euler(0, 0, 0)
+                marker = Marker()
+                marker.header.frame_id = 'map'
+                marker.header.stamp = rospy.Time.now()
+                marker.id = int(cell.centerx * 100000000 + cell.centery * 100)
+                marker.type = 1
+                marker.action = 0
+                marker.scale.x = self.navigationmap.scanwidthinmeters
+                marker.scale.y = self.navigationmap.scanwidthinmeters
+                marker.scale.z = self.navigationmap.scanwidthinmeters
+                marker.pose.position.x = cell.centerx
+                marker.pose.position.y = cell.centery
+                marker.pose.position.z = 0.0
+                marker.pose.orientation.x = quat[0]
+                marker.pose.orientation.y = quat[1]
+                marker.pose.orientation.z = quat[2]
+                marker.pose.orientation.w = quat[3]
+                marker.color.a = 0.25
+
+                if cell.status == GridCellStatus.FREE:
+                    marker.color.r = 0
+                    marker.color.g = 1
+                    marker.color.b = 0
+                elif cell.status == GridCellStatus.OCCUPIED:
+                    marker.color.r = 1
+                    marker.color.g = 0
+                    marker.color.b = 0
+
+                markers.markers.append(marker)
+
+        image = self.navigationmap.toDebugImage()
+        cv2.imwrite('../maps/loaded.png', image)
+
+        mappub = rospy.Publisher('map', OccupancyGrid, queue_size=10)
+        mappub.publish(map)
+
+        self.markerstopic = rospy.Publisher('visualization_marker', MarkerArray, queue_size=10, latch=True)
+        self.markerstopic.publish(markers)
+
+        rospy.loginfo('Generated %s markers', len(markers.markers))
+
+        rospy.Subscriber("move_base_simple/goal", PoseStamped, self.newMoveBaseSimpleGoalMessage)
+
+        # Processing the sensor polling in an endless loop until this node shuts down
+        rate = rospy.Rate(10)
+
+        while not rospy.is_shutdown():
+            rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        m = ReadMap()
+        m.start()
+    except rospy.ROSInterruptException:
+        pass
