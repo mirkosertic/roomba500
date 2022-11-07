c43a1af88d9d76696de49ee46b91e3ef236a1162
mirkosertic <mirko.sertic@web.de>
2022-11-07 10:26:06 +0100

    Bug fix in cleaning area calculation

--- src/magnetometer.py
+++ src/magnetometer.py
@@ -6,6 +6,8 @@ import math
 from std_msgs.msg import Int16
 from sensor_msgs.msg import MagneticField
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point32
+from sensor_msgs.msg import PointCloud
 from tf.transformations import quaternion_from_euler
 
 from HMC5883L import HMC5883L
@@ -20,6 +22,7 @@ class Magnetometer:
         self.magneticfieldframe = None
         self.hmc5883l = None
         self.odomframe = 'odom'
+        self.debugpointcloudpub = None
 
     def newShutdownCommand(self, data):
         rospy.signal_shutdown('Shutdown requested')
@@ -48,6 +51,16 @@ class Magnetometer:
 
         self.magneticfieldodompub = rospy.Publisher('odom', Odometry, queue_size=10)
 
+        self.debugpointcloudpub = rospy.Publisher('imu/mag_debugpoints', PointCloud, queue_size=10)
+
+        minx = None
+        miny = None
+        maxx = None
+        maxy = None
+
+        debugdata = PointCloud()
+        debugdata.header.frame_id = self.magneticfieldframe
+
         # Processing the sensor polling in an endless loop until this node shuts down
         rospy.loginfo("Polling magnetometer..")
         while not rospy.is_shutdown():
@@ -61,10 +74,31 @@ class Magnetometer:
             magmessage.header.stamp = currenttime
 
             if x is not None:
+                if maxx is None:
+                    maxx = x
+                else:
+                    maxx = max(x, maxx)
+
+                if minx is None:
+                    minx = x
+                else:
+                    minx = min(x, minx)
+
                 magmessage.magnetic_field.x = x
             if y is not None:
+                if maxy is None:
+                    maxy = y
+                else:
+                    maxy = max(y, maxy)
+
+                if miny is None:
+                    miny = y
+                else:
+                    miny = min(y, miny)
+
                 magmessage.magnetic_field.y = y
             if z is not None:
+
                 magmessage.magnetic_field.z = z
 
             self.magneticfieldpub.publish(magmessage)
@@ -88,6 +122,15 @@ class Magnetometer:
                 odommessage.pose.pose.orientation.w = q[3]
 
                 self.magneticfieldodompub.publish(odommessage)
+
+                debugdata.header.stamp = rospy.Time.now()
+
+                # Make sure memory is not exploding
+                if len(debugdata.points) > 1000:
+                    debugdata.points.pop(0)
+
+                debugdata.points.append(Point32(x, y, 0))
+                self.debugpointcloudpub.publish(debugdata)
 
             rate.sleep()
 
