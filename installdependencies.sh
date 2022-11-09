48de67ec0632e75ab67f75fbe23fa54d8501cbe0
mirkosertic <mirko.sertic@web.de>
2022-11-09 13:05:43 +0100

    Working on better calibration support

---
+++ installdependencies.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo apt install curl
+sudo apt install ros-noetic-desktop-fullsudo apt install ros-noetic-desktop-full
+sudo apt install ros-noetic-desktop-full
+sudo apt-get install ros-noetic-imu-tools
+sudo apt-get install ros-noetic-plotjuggler-ros ros-noetic-plotjuggler
+sudo apt-get install ros-noetic-robot-pose-ekf
+sudo apt-get install ros-noetic-robot-localization
+sudo apt-get install ros-noetic-navigation
+sudo apt-get install ros-noetic-gmapping ros-noetic-amcl
+sudo apt-get install pip3
+sudo apt-get install python3-pip
+sudo pip3 install starlette uvicorn
+sudo pip3 install luma.emulator luma
+sudo pip3 install sse_starlette
+sudo pip3 install luma.oled
+sudo apt-get install ros-noetic-turtlebot3 ros-noetic-turtlebot3-bringup
+sudo apt-get install ros-noetic-turtlebot3-gazebo
+sudo apt-get install ros-noetic-turtlebot3-gazebo
+sudo apt-get install ros-noetic-gazebo-ros-pkgs
+sudo apt-get install ros-noetic-ifopt
+sudo apt-get install ros-noetic-global-planner
+sudo apt-get install ros-noetic-slam-toolbox
